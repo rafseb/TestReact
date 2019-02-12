6e3d4b2086891c457c9e1cb204572f5119b7207c
rafseb <45681001+rafseb@users.noreply.github.com>
2019-02-12 21:55:00 +0800

    component files
    initial component files

---
+++ StorePicker.js
@@ -0,0 +1,16 @@
+import React, { Fragment } from 'react';
+import { getFunName } from "../helpers";
+
+class StorePicker extends React.Component {
+    render(){
+        return (
+            <from className="store-selector">
+                <h2>Please Enter A Store</h2>
+                <input type="text" required placeholder="Store Name" defaultValue={ getFunName() } />
+                <button type="submit">Visit Store →</button>
+            </from>
+        )
+    }
+}
+
+export default StorePicker;
